be8145f8a4b84c0828f1639a6bd6cf033d2d5f72
PABLO TALONIA <94588789+Pablolsd25@users.noreply.github.com>
2024-03-12 17:46:12 +0000

    Template terminado

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,5 +1,36 @@
+import Container from "../components/ui/Container";
+import Card, { CardTemplate } from "../components/ui/CardTemplate";
 function AboutPage() {
-  return <div>AboutPage</div>;
+  return (
+    <Container>
+      <CardTemplate>
+        <div className="max-w-2xl mx-auto ">
+          <h1 className="text-4xl text-center font-bold text-orange-600 mb-8">
+            Sobre Nosotros
+          </h1>
+          <p className="text-gray-700 text-lg mb-8">
+            En Yum Kaax, nos dedicamos a ofrecerte los mejores productos
+            naturales y de calidad. Nuestro compromiso es proporcionarte una
+            experiencia única y saludable.
+          </p>
+          <p className="text-gray-700 text-lg mb-8">
+            Con años de experiencia en la industria, hemos perfeccionado
+            nuestras mezclas para asegurarnos de que cada producto cumpla con
+            los más altos estándares de calidad y sabor.
+          </p>
+          <p className="text-gray-700 text-lg mb-8">
+            Nuestra misión es brindarte productos que no solo sean buenos para
+            ti, sino también buenos para el planeta. Nos esforzamos por utilizar
+            ingredientes naturales y sostenibles en todos nuestros productos.
+          </p>
+          <p className="text-gray-700 text-lg">
+            ¡Gracias por ser parte de nuestra comunidad y apoyar nuestro
+            trabajo!
+          </p>
+        </div>
+      </CardTemplate>
+    </Container>
+  );
 }
 
 export default AboutPage;
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,5 +1,36 @@
+import Container from "../components/ui/Container";
+import CardTemplate from "../components/ui/CardTemplate";
+
 function ContactPage() {
-  return <div>ContactPage</div>;
+  return (
+    <Container>
+      <CardTemplate>
+        <div className="max-w-2xl mx-auto py-12">
+          <h1 className="text-4xl text-center font-bold text-orange-600 mb-8">
+            Contáctanos
+          </h1>
+          <p className="text-gray-700 text-lg mb-8">
+            Si tienes alguna pregunta, comentario o sugerencia, no dudes en
+            ponerte en contacto con nosotros. Estamos aquí para ayudarte en lo
+            que necesites.
+          </p>
+          <p className="text-gray-700 text-lg mb-8">
+            Puedes comunicarte con nosotros a través del siguiente correo
+            electrónico:{" "}
+            <a href="mailto:info@example.com" className="text-blue-600">
+              info@example.com
+            </a>
+            .
+          </p>
+          <p className="text-gray-700 text-lg mb-8">
+            También puedes encontrarnos en nuestras redes sociales. ¡Síguenos
+            para mantenerte al tanto de nuestras últimas novedades y
+            promociones!
+          </p>
+        </div>
+      </CardTemplate>
+    </Container>
+  );
 }
 
 export default ContactPage;
